2b0926e3844d5a196213a27793b0eb29e8be48cf
TonyGrif <TonyGriffin2000@gmail.com>
2024-08-15 11:57:31 -0400

    feat: add requests and bs4 libraries, set up charts data structure

---
+++ billboard/charts.py
@@ -0,0 +1,79 @@
+from typing import Optional
+
+
+class BillboardChart:
+    """
+    A data structure representing a week of Billboard Hot 100's charts.
+
+    Attributes
+    -----------
+    date: str
+        The date for this chart in ISO 8601 format (YYYY-MM-DD).
+    chart: List[]
+        The chart for the given date.
+    """
+
+    def __init__(self, date: Optional[str] = None) -> None:
+        """
+        The constructor for a BillboardChart object.
+
+        Parameters
+        -----------
+        date: str
+            An optional date (YYYY-MM-DD) for this chart; if none is provided,
+            the current date is used.
+        """
+        raise NotImplementedError
+
+    @property
+    def date(self) -> str:
+        """
+        Get the data used for the current chart.
+
+        Returns
+        --------
+        str
+            The ISO 8601 formatted date.
+        """
+        raise NotImplementedError
+
+    @date.setter
+    def date(self, iso_date: str) -> None:
+        """
+        Set a new date for the class and update the current chart.
+
+        Parameters
+        -----------
+        iso_date: str
+            A ISO 8601 formatted date.
+
+        Raises
+        --------
+        ValueError
+            If the date is before 1958-08-04 or after the current date.
+        """
+        raise NotImplementedError
+
+    @property
+    def charts(self):
+        """
+        Get the full chart from the given week.
+
+        Returns
+        ---------
+        List
+            A collection of chart data from the given week.
+        """
+        raise NotImplementedError
+
+    @property
+    def top_spot(self):
+        """
+        Get the top spot from this week.
+
+        Returns
+        --------
+        TBD
+            A data structure containing the top spot information.
+        """
+        raise NotImplementedError
