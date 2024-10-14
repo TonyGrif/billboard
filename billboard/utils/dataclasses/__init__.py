23cf6ac565151de4a38a264cd964bcd9afa2decb
TonyGrif <TonyGriffin2000@gmail.com>
2024-10-14 17:40:12 -0400

    ref: update artist module to use the utilities module

--- billboard/utils/dataclasses/__init__.py
+++ billboard/utils/dataclasses/__init__.py
@@ -1,5 +1,11 @@
+"""
+This module contains dataclasses for use by Scrapers.
+"""
+
+from .artist_entry import ArtistEntry
 from .song_entry import SongEntry
 
 __all__ = [
     "SongEntry",
+    "ArtistEntry",
 ]
