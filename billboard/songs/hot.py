1801f2dada64b23cedb16d0891da9942d6e8ad64
TonyGrif <TonyGriffin2000@gmail.com>
2024-10-14 17:49:13 -0400

    chore: rename song parser for clarity

--- billboard/songs/hot.py
+++ billboard/songs/hot.py
@@ -3,7 +3,7 @@
 from datetime import datetime, timedelta
 from typing import Optional
 
-from billboard.utils import make_request, parse_request
+from billboard.utils import make_request, parse_song_request
 
 from .super import SongChart
 
@@ -40,7 +40,7 @@ class BillboardChart(SongChart):
         Generate the chart for the given week.
         """
         response = make_request("hot-100", self.date)
-        if (data := parse_request(response)) == [] and self.auto_date is True:
+        if (data := parse_song_request(response)) == [] and self.auto_date is True:
             week_ago = datetime.fromisoformat(self.date) - timedelta(weeks=1)
             self.date = week_ago.strftime("%Y-%m-%d")
         else:
--- billboard/utils/__init__.py
+++ billboard/utils/__init__.py
@@ -14,12 +14,12 @@ Functions
 """
 
 from .dataclasses import ArtistEntry, SongEntry
-from .funcs import make_request, parse_artist_request, parse_request
+from .funcs import make_request, parse_artist_request, parse_song_request
 
 __all__ = [
     "ArtistEntry",
     "SongEntry",
     "make_request",
-    "parse_request",
     "parse_artist_request",
+    "parse_song_request",
 ]
