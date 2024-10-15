0f655c52ab0a1f6bd315fd9ca50846abaec70c8e
TonyGrif <TonyGriffin2000@gmail.com>
2024-10-15 13:00:34 -0400

    docs: update artist module to google docstrings

--- billboard/artists/artist.py
+++ billboard/artists/artist.py
@@ -1,4 +1,4 @@
-"""This module contains the scraper for the Aritst 100 chart."""
+"""This module contains the scraper for the Artist 100 chart."""
 
 from datetime import datetime, timedelta
 from typing import Optional
@@ -10,46 +10,37 @@ from billboard.utils import make_request, parse_request
 
 class ArtistChart(Chart):
     """
-    A data structure representing a week of the artist 100 chart.
+    A data structure representing a week of the Artist 100 Billboard Chart.
 
-    Attributes
-    ------------
-    date: str
-        The date for this chart in ISO 8601 format (YYYY-MM-DD).
-    chart: List[ChartEntry]
-        The chart for the given date containing all chart data.
+    Attributes:
+        date: The date for this chart in ISO 8601 format (YYYY-MM-DD).
+        chart: The chart for the given date containing all chart data.
+        auto_date: Determines if the object will auto update the date to the
+            previous week if the chosen one does not exist.
+        oldest_date: The oldest date allowed for a given chart.
     """
 
     def __init__(self, date: Optional[str] = None, auto_date: bool = True) -> None:
-        """
-        The constructor for a BillboardChart object.
+        """The constructor for a ArtistChart object.
 
-        Parameters
-        -----------
-        date: str
-            An optional date (YYYY-MM-DD) for this chart; if none is provided,
-            the chart from one day ago is used.
-        auto_date: bool
-            Determines if the object will auto update the date to the previous
-            week if the choosen one does not exist.
+        Args:
+            date: An optional date (YYYY-MM-DD); if none is provided, yesterday is used.
+            auto_date: Determines if the object will auto update the date to the
+                previous week if the choosen one does not exist.
         """
         super().__init__(date, auto_date, "2014-07-19")
 
     @property
     def top_spot(self) -> ArtistEntry:
-        """
-        Get the top spot from this week.
+        """Get the top spot from this week.
 
-        Returns
-        --------
-        ArtistEntry
+        Returns:
             A data structure containing the top spot information.
         """
         return self.chart[0]
 
     def _generate_chart(self):
-        """
-        Generate the chart for the given week.
+        """Generate the chart for the given week.
         """
         response = make_request("artist-100", self.date)
         if (data := parse_request(response)) == [] and self.auto_date is True:
